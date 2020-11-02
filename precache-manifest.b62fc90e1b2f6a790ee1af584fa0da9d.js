4fe7132b25d0250d1c1fc037f42e5fdf65a62263
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-11-02 10:46:47 +0100

    Updates

---
+++ precache-manifest.b62fc90e1b2f6a790ee1af584fa0da9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff504044eb1c140dcb2786a77c4e117",
+    "url": "//wiemon.github.io/to-do-app-react/index.html"
+  },
+  {
+    "revision": "62260477e77f853755da",
+    "url": "//wiemon.github.io/to-do-app-react/static/css/main.e66b5095.chunk.css"
+  },
+  {
+    "revision": "98c0ced9c40d2a1ca305",
+    "url": "//wiemon.github.io/to-do-app-react/static/js/2.79c5faf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//wiemon.github.io/to-do-app-react/static/js/2.79c5faf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62260477e77f853755da",
+    "url": "//wiemon.github.io/to-do-app-react/static/js/main.566b4e68.chunk.js"
+  },
+  {
+    "revision": "0939f03f39ace3287c01",
+    "url": "//wiemon.github.io/to-do-app-react/static/js/runtime-main.20bcba7e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "//wiemon.github.io/to-do-app-react/precache-manifest.b62fc90e1b2f6a790ee1af584fa0da9d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//wiemon.github.io/to-do-app-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
